24172c8ce1c5479b955e240700312fb92aeea478
root <root@localhost>
2013-12-11 20:12:19 +0000

    Merged commit of all code and modules

---
+++ sites/all/modules/custom/custom_mtech/js/custom_mtech.js
@@ -0,0 +1,19 @@
+/**
+ * @file
+ * The JavaScript behaviours for custom_mtech.
+ */
+(function ($) {
+  /**
+   * Register the custom_mtech behavior.
+   */
+  Drupal.behaviors.custom_mtech = {
+    attach: function (context, settings) {
+    }
+  };
+
+  /**
+   * Class containing functionality for custom_mtech.
+   */
+  Drupal.custom_mtech = {};
+
+})(jQuery);
